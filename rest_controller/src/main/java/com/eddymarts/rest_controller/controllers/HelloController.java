1c517e3b19cdf4ec2d97b619a68144401619929f
Eddy Martínez <emartinez@restrata.com>
2021-08-19 18:21:55 +0100

    Moves package to correct location.

---
+++ rest_controller/src/main/java/com/eddymarts/rest_controller/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package com.eddymarts.rest_controller.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	// http://localhost:8080/hello?name=Eddy
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value="name", defaultValue="World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
